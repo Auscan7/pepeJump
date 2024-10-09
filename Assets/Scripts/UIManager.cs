b7b7e346c647ca24ba3377feb5e6eb6f55ac4e48
Auscan7 <auscan7@gmail.com>
2024-10-09 15:44:09 +0300

    UI changes

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,10 +6,13 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance; // Singleton instance
     public GameObject levelSelectionPanel; // Panel to show level selection
+    public GameObject upgradePanel; // Panel to show upgrade buttons
     public Button toggleButton; // Button to toggle the level selection panel
+    public Button upgradeToggleButton; // Button to toggle the upgrade panel
     public Button[] levelButtons; // Buttons for each level
 
     private bool isPanelActive = false;
+    private bool isUpgradePanelActive = false;
 
     private void Awake()
     {
@@ -28,9 +31,12 @@ public class UIManager : MonoBehaviour
     {
         // Ensure the panel is initially inactive
         levelSelectionPanel.SetActive(false);
+        upgradePanel.SetActive(false);
+
 
         // Add listener to the toggle button to open/close the panel
         toggleButton.onClick.AddListener(ToggleLevelSelection);
+        upgradeToggleButton.onClick.AddListener(ToggleUpgradeSelection);
 
         // Update the level buttons according to unlocked levels
         UpdateLevelButtons();
@@ -48,6 +54,18 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    private void ToggleUpgradeSelection()
+    {
+        isUpgradePanelActive = !isUpgradePanelActive;
+        upgradePanel.SetActive(isUpgradePanelActive);
+
+        if (!isUpgradePanelActive)
+        {
+            // Deselect any currently selected UI element
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+
     public void ShowLevelSelection()
     {
         // Ensure the panel is active
@@ -58,10 +76,23 @@ public class UIManager : MonoBehaviour
         UpdateLevelButtons();
     }
 
+    public void ShowUpgradelSelection()
+    {
+        // Ensure the panel is active
+        upgradePanel.SetActive(true);
+        isUpgradePanelActive = true;
+    }
+
     public void CloseLevelSelection()
     {
         levelSelectionPanel.SetActive(false);
         isPanelActive = false;
+    }
+
+    public void CloseUpgradeSelection()
+    {
+        upgradePanel.SetActive(false);
+        isUpgradePanelActive = false;
     }
 
     public void UpdateLevelButtons()
